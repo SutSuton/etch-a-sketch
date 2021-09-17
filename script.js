04b5b39c8b13c5753d093f04143b2704915c2412
sutsuton <sutsuton@gmail.com>
2021-09-17 17:04:56 +1000

    Added interactivity with the grid dimensions and fixed up general page layout. MouseEnter events only fire once on the very first cell so still have some fixing up to do.

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const container = document.querySelector(".grid-container");
 
+let n = 10;
 
-for (let i = 1; i <= 16; i++) {
-    let i = document.createElement("div");
-    container.appendChild(i);
-    i.textContent = "hello";
-    i.className = "cell";
+makeGrid(n);
+
+function makeGrid(rows) {
+    let idNumber = 0;
+    for (let i = 1; i <= rows ** 2; i++) {
+        let i = document.createElement("div");
+        container.appendChild(i);
+        i.className = "cell";
+        idNumber += 1;
+        i.id = "cell" + idNumber;
+    }
 }
+
+container.style.setProperty('--grid-columns', n);
+container.style.setProperty('--grid-rows', n);
+
+//function changeColor() {
+//    cell.style.setProperty('--cell-color', 'blue');
+//}
+
+const cell = document.querySelector(".cell");
+
+cell.addEventListener('mouseover', e => {
+    cell.style.setProperty('--cell-color', 'blue');
+    console.log('hey');
+});
