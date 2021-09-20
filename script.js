2f737abb7ed90c834e8fa1c61762499d4a7101cb
sutsuton <sutsuton@gmail.com>
2021-09-20 16:21:55 +1000

    Added user functionality with custom grid sizes. Cleaned up function names. Added a new function clearGrid() for removing all elements within the parent container before building a new grid. Added rudimentary support for changing colour but have yet to implement it in a meaningful way

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const container = document.querySelector(".grid-container");
-const cell = container.childNodes;
+let cell = container.childNodes;
+const root = document.documentElement;
 
-let n = 8; //for now, will add a user interface to this later
+let n = 16
 makeGrid(n);
+enableDraw('black');
 
-function makeGrid(rows) {
-    let idNumber = 0;
-    for (let i = 1; i <= rows ** 2; i++) {
-        let i = document.createElement("div");
-        container.appendChild(i);
-        i.className = "cell";
-        idNumber += 1;
-        i.id = "cell" + idNumber;
-    }
+function getInputValue() {
+  let n = document.getElementById("rows").value;
+  if  (n < 1 || n > 100) alert("Please input a number between 1 and 100");
+  else if (n != Math.floor(n)) alert("Input must be a whole number");
+  else {
+    clearGrid();
+    makeGrid(n);
+    enableDraw('black');
+  }
 }
 
-container.style.setProperty('--grid-columns', n);
-container.style.setProperty('--grid-rows', n);
+function clearGrid() {
+  container.innerHTML = "";
+}
 
-cell.forEach(item => {
+function makeGrid(rows) {
+  container.style.setProperty('--grid-columns', rows);
+  container.style.setProperty('--grid-rows', rows);
+  for (let i = 1; i <= rows ** 2; i++) {
+    let newCell = document.createElement("div");
+    container.appendChild(newCell);
+    newCell.className = "cell";
+  }
+}
+
+function reset() {
+  cell.forEach(item => {
+    item.classList.remove("colored");
+    item.className = "cell";
+  })
+}
+
+
+function enableDraw(color) {
+  cell.forEach(item => {
     item.addEventListener('mouseover', e => {
       item.className = "colored";
+      root.style.setProperty('--cell-color', color)
     })
   })
+}
